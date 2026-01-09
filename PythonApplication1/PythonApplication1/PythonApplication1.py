e42bd2156083f2f268e80d260c58e17ee23a2d7b
madison <mcsch201@learner.dvusd.org>
2026-01-09 12:39:42 -0700

    pyfest (initials)

---
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,12 @@
+# The initial lineup
+lineup = [
+    ("Code Play", "Indie", 30),
+    ("The Pythonistas", "Rock", 45),
+    ("Syntax Error", "Metal", 60)
+]
+
+# 1. Add the headliner
+headliner = ("The Byte Beats", "Synthwave", 90)
+lineup.append(headliner)
+
+# Continue the logic below...
